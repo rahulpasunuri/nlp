65b996fc0de856a01bd4ee7a0eb8c1cc63a26fc8
Rahul Pasunuri <rahul.pasunuri@gmail.com>
2014-09-23 20:45:40 -0400

    pruned input text

--- sol2/3gram.py
+++ sol2/3gram.py
@@ -6,11 +6,22 @@ trigramCounts={}
 bigramCounts={}
 conditionalProbabilities={}
 
+def pruneText(s):
+	s=s.replace('.','')
+	s=s.replace(',',"")
+	s=s.replace("?","")
+	s=s.replace("\"","")
+	s=s.replace("\'","")
+	s=s.replace("-","")	
+	return s
+	
+	
+	
 def main():
 	#(w1,w2,w3,count)--is the structure of the trigrams
 	f=open("hw-train.txt", 'r')
 	sentences=f.readlines()	
-	sentences=[(" <s1> <s2> "+s+" </s2> </s1> ").replace('.','').replace(',',"").split() for s in sentences]
+	sentences=[(" <s1> <s2> "+pruneText(s)+" </s2> </s1> ").split() for s in sentences]
 
 	#compute bigram counts
 	for s in sentences:
@@ -39,7 +50,7 @@ def main():
 	#compute conditional probabilities
 	for key in trigramCounts:
 		conditionalProbabilities[key] = float(trigramCounts[key])/bigramCounts[(key[0]),key[1]]
-	
+	#print conditionalProbabilities
 	#compute probabilities on the training set
 
 #Execution begins here
