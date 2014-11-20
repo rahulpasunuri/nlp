8a4856b1fb660ceb95fbc58685afcfd4d755f75b
hydargah <hydargah@indiana.edu>
2014-11-20 00:34:05 -0500

    Adding the gitignore file

--- project/eg1.py
+++ project/eg1.py
@@ -3,18 +3,33 @@ import io
 
 def main():
 	reviewDirectory = "nyu\\PROJECTS\\Epicurious\\DATA\\reviews\\"
+	ratingList = []
+	reviewList = []	
+
 	fileList = os.listdir(reviewDirectory)
+	
 	for f in fileList:
-		currentFile = io.open(reviewDirectory+f,newline="")
+		currentFile = io.open(reviewDirectory+f,newline="",encoding="cp437")
 		recipeName = getRecipeName(f)
 		print
-		print recipeName
 		for line in currentFile:
 			if line=="\n":
 				continue
-			print line.encode('utf-8')
-			print "-------------"
-		
+			if "Rating" in line:
+				rating = line[len(line)-2]
+				print  recipeName+" = "+rating
+				try:
+					rating = int(rating)
+				except :
+					continue	
+				ratingList.append(int(rating))
+			else : 
+				try:
+					review = line.encode('utf-8')
+					reviewList.append(str(review))
+				except :
+					ratingList.pop();		
+					continue
 
 def getRecipeName(rawRecipeName):
 	startIndex = rawRecipeName.rfind('_');
