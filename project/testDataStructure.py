c63850a496643960c8c3c8b076e0ec2f0d136d03
Rahul Pasunuri <rahul.pasunuri@gmail.com>
2014-12-01 21:44:30 -0500

    updating the test file

--- project/testDataStructure.py
+++ project/testDataStructure.py
@@ -5,12 +5,15 @@ def main():
 	allReviews = pickle.load(pickleFile)
 
 	for obj in allReviews:
-		print obj.name+" --> "+str(obj.rating)+ " Number of Reviews --> "+ str(obj.numOfReviews)
+		#print obj.name+" --> "+str(obj.rating)+ " Number of Reviews --> "+ str(obj.numOfReviews)
 		#print obj.topicProbabilities
 		#print obj.reviewRatings
-		print "Length of topic topicProbabilities : "+str(len(obj.topicProbabilities))
-		print "Length of review rating: "+str(len(obj.reviewRatings))
-		print "*"*40
+		#print "Length of topic topicProbabilities : "+str(len(obj.topicProbabilities))
+		#print "Length of review rating: "+str(len(obj.reviewRatings))
+		#print "*"*40
+		if len(obj.topicProbabilities) != obj.numOfReviews:
+			print "Error"
+		
 
 
 
