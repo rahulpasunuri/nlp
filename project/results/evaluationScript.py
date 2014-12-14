68eeb215c45730318c7ddb83bc6a0b15dc78d1ef
Rahul Pasunuri <rahul.pasunuri@gmail.com>
2014-12-14 00:46:56 -0500

    adding results

---
+++ project/results/evaluationScript.py
@@ -0,0 +1,12 @@
+
+def main():
+	predictionsFile = "svm results/results5.txt"
+	predictionsFile = "svm results/results10.txt"
+	
+	actualFile = "/svm results/vectors/testVectors5.txt"
+	
+	tpOne = 0
+	
+	
+
+if __name__ == "__main__": main()
